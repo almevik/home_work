1f6ebf6df42930d9cede70fc8b15d9e73f55d49f
almevik <almevik@mail.ru>
2021-04-23 13:40:14 +0300

    ДЗ12 Логирование http запросов.

--- hw12_13_14_15_calendar/internal/server/http/server.go
+++ hw12_13_14_15_calendar/internal/server/http/server.go
@@ -70,23 +70,6 @@ func (s *Server) configureRouter() {
 	s.router.HandleFunc("/hello-world", s.homeHandler).Methods("GET")
 }
 
-func (s *Server) loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		rw := &responseWriter{w, http.StatusOK}
-		next.ServeHTTP(rw, r)
-
-		s.logger.Info(
-			fmt.Sprintf("%s %s %s %s %d %s",
-				requestAddr(r),
-				r.Method,
-				r.RequestURI,
-				r.Proto,
-				rw.code,
-				userAgent(r),
-			))
-	})
-}
-
 func requestAddr(r *http.Request) string {
 	return strings.Split(r.RemoteAddr, ":")[0]
 }
